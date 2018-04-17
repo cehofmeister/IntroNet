365fe5b71b8514759827f4b21b7dd7f578bada23
Jimmy <jimmay.schraeder@gmail.com>
2018-04-17 14:41:58 -0400

    able to now create a new conference.

--- IntroNet-WebServer/classes/pages/NewConferencePage.php
+++ IntroNet-WebServer/classes/pages/NewConferencePage.php
@@ -7,14 +7,38 @@ require_once './classes/components/Form.php';
 
 class NewConferencePage extends Page {
     const UserType = "Planner";
+
+    public function callBack($data, $action, \PageBody &$body) {
+        //$body->addToTop(new Message("Please fill all the information", Message::DANGER));
+        // get number of rounds
+
+        $body->addToTop(new Message("'{$data["conferenceName"]}'", Message::SUCCESS));
+        $body->addToTop(new Message("'".date('Y-m-d', strtotime(str_replace('-', '/',  $data["eventStart"])))."'", Message::SUCCESS));
+        $body->addToTop(new Message("'{$data["eventStartTime"]}'", Message::SUCCESS));
+        $body->addToTop(new Message("'".date('Y-m-d', strtotime(str_replace('-', '/',  $data["eventDeadline"])))."'", Message::SUCCESS));
+        $body->addToTop(new Message("'{$data["eventDeadlineTime"]}'", Message::SUCCESS));
+
+        $conference_id = Database::insert("Conference", array(
+            "Conference_name"             => "'{$data["conferenceName"]}'",
+            "registration_start_date"     => "'".date('Y-m-d', strtotime(str_replace('-', '/',  $data["eventStart"])))."'",
+            "registration_start_time"     => "'{$data["eventStartTime"]}'",
+            "registration_deadline_date"  => "'".date('Y-m-d', strtotime(str_replace('-', '/',  $data["eventDeadline"])))."'",
+            "registration_deadline_time"  => "'{$data["eventDeadlineTime"]}'",
+
+        ));
+
+        $body->addToTop(new Message("<h3>Conference was created</h3>"
+        . "<a href='?page=Conference&conference=$conference_id'>Click Here to View Conference's Details</a>", Message::SUCCESS));
+    }
+
     protected function build(PageBody &$body, SubMenu &$submenu) {
         $this->pageName = "Conference";
         
         
-        $form = new Form("Conference");
+        $form = new Form("NewConference");
         
         
-        $form->addInput(Input::textInput("Conference Name", "Conference Name",'', True));
+        $form->addInput(Input::textInput("conferenceName", "Conference Name",'', True));
         
         $form->addInput(Input::createGroupInput(array(
             Input::dateInput("eventStart","Registration Start Date",'', True),
